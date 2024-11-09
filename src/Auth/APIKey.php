d2eddd113d72e56f9e071067b6d64f3e0ad48ee7
coolxitech <admin@sorpack.cn>
2024-11-09 10:21:47 +0800

    only support apiKey

--- src/Auth/APIKey.php
+++ src/Auth/APIKey.php
@@ -9,20 +9,16 @@ namespace Cloudflare\API\Auth;
 
 class APIKey implements Auth
 {
-    private $email;
     private $apiKey;
 
-    public function __construct(string $email, string $apiKey)
+    public function __construct(string $apiKey)
     {
-        $this->email  = $email;
         $this->apiKey = $apiKey;
     }
 
     public function getHeaders(): array
     {
         return [
-            'X-Auth-Email'   => $this->email,
-            'X-Auth-Key' => $this->apiKey,
             'Authorization' => 'Bearer '.$this->apiKey,
         ];
     }
